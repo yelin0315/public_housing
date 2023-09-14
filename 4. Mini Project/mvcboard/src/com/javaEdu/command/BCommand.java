b19f45b39bb1b0f4d1ffb06f4d379d1c32022a74
yelin_park <131628613+yelin0315@users.noreply.github.com>
2023-09-14 11:20:02 +0900

    Delete 4. Mini Project/mvcboard directory

--- 4. Mini Project/mvcboard/src/com/javaEdu/command/BCommand.java
+++
@@ -1,9 +0,0 @@
-package com.javaEdu.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BCommand {
-	
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
